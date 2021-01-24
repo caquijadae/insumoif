22850c7ef7ba1ab436d55404a993d96cff6ada0d
Cristián <caayala@gmail.com>
2021-01-24 12:24:38 -0300

    agregar rlang como dependencia

--- R/analisis_muestra.R
+++ R/analisis_muestra.R
@@ -6,6 +6,7 @@
 #' @param var2 variable a analizar, multiplicada por el factor de expansion
 #'
 #' @return un data frame, con los descriptivos (n, suma, media, varianza, minimo, maximo, suma expandida, media expandida) de la variable a analizar, ademas del folio a eliminar y su respectivo valor de la variable analizada. Cada fila es un calculo distinto: el primero incluye todas las unidades, el segundo excluye la unidad con mayor valor, el tercero las dos unidades con mayor valor, asi sucesivamente hasta llegar a n=1
+#' @importFrom rlang .env
 #' @export
 #'
 #' @examples analisis_muestra(base1 = LifeCycleSavings, id = sr, var1 = pop15, var2 = dpi)
